5549b803cf65606e1357fa6bcddbd16fcb134e82
Raghu K <raghuk.vit@gmail.com>
2022-11-19 06:45:25 +0530

    Update tf-module-docdb/

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "aws_docdb_cluster" "docdb" {
   depends_on             = [aws_docdb_subnet_group.default]
   cluster_identifier     = "${var.env}-${var.name}-roboshop-docdb"
   engine                 = var.engine
-  master_username        = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCDB_USER"]
-  master_password        = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["DOCDB_PASS"]
+  master_username        = local.DOCDB_USER
+  master_password        = local.DOCDB_PASS
   skip_final_snapshot    = var.skip_final_snapshot
   db_subnet_group_name   = "${var.env}-${var.name}-roboshop-docdb"
   vpc_security_group_ids = [aws_security_group.sg.id]
